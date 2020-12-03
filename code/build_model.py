c690f57ee7d0a1cc94401b74976ced9d8f90eb2c
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2020-12-03 17:09:54 +0100

    Add model build capability

---
+++ code/build_model.py
@@ -0,0 +1,195 @@
+import logging
+import os
+import shutil
+import warnings
+import zipfile
+from pathlib import Path
+
+import cv2
+import numpy as np
+import pytorch_lightning as pl
+import splitfolders
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from tqdm import tqdm
+
+from model import LitModel, hparams
+
+# Some of the methods we call have changed the default behavior and indicate this with a warning.
+# Since the changes are not relevant for us, we hide the warnings.
+warnings.filterwarnings("ignore")
+
+# Logging config
+log = logging.getLogger(__file__)
+log.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+log.addHandler(ch)
+
+
+def cal_dir_stat(root: str, filetype: str) -> tuple:
+    """
+    Calculates the mean and standard deviation per channel for all images in the
+    passed dateset. Do not calculate the statistics on the whole dataset, 
+    as per here http://cs231n.github.io/neural-networks-2/#datapre.
+    
+    root:
+        Path to dataset
+    filetype:
+        Type of images in dataset, e.g. ppm or jpg
+        
+    """
+    log.info("Calculate mean/std values")
+    CHANNEL_NUM = 3
+    cls_dirs = [x for x in Path(root).iterdir() if x.is_dir()]
+    pixel_num = 0  # store all pixel number in the dataset
+    channel_sum = np.zeros(CHANNEL_NUM)
+    channel_sum_squared = np.zeros(CHANNEL_NUM)
+
+    for idx, d in enumerate(cls_dirs):
+        for image in tqdm(d.glob(f"*.{filetype}"), total=len(cls_dirs)):
+            im = cv2.imread(
+                str(image)
+            )  # image in M*N*CHANNEL_NUM shape, channel in BGR order
+            im = im / 255.0
+            pixel_num += im.size / CHANNEL_NUM
+            channel_sum += np.sum(im, axis=(0, 1))
+            channel_sum_squared += np.sum(np.square(im), axis=(0, 1))
+
+    bgr_mean = channel_sum / pixel_num
+    bgr_std = np.sqrt(channel_sum_squared / pixel_num - np.square(bgr_mean))
+
+    # change the format from bgr to rgb
+    rgb_mean = list(bgr_mean)[::-1]
+    rgb_std = list(bgr_std)[::-1]
+
+    return rgb_mean, rgb_std
+
+
+def __unzip_file(filepath, target="/content/"):
+    filepath = Path(filepath)
+    with zipfile.ZipFile(filepath, "r") as zip_ref:
+        for file in zip_ref.namelist():
+            if file.startswith(filepath.stem + "/"):
+                zip_ref.extract(file, target)
+
+    # Remove annoying child folder...
+    file_names = os.listdir(Path(target).joinpath(filepath.stem))
+
+    for file_name in file_names:
+        shutil.move(
+            os.path.join(Path(target).joinpath(filepath.stem), file_name), target
+        )
+
+    shutil.rmtree(Path(target).joinpath(filepath.stem))
+
+
+def _unzip_file(filepath, target="/content/"):
+    filepath = Path(filepath)
+
+    with zipfile.ZipFile(filepath, "r") as zip_ref:
+        for file in tqdm(zip_ref.namelist()):
+            folder_name = filepath.stem + "/"
+            if file.startswith(folder_name) and not file == folder_name:
+                tmp_target = Path(Path(target).joinpath(file))
+                tmp_target.parent.mkdir(exist_ok=True, parents=True)
+
+                if file.endswith("/"):
+                    tmp_target.mkdir(exist_ok=True, parents=True)
+                else:
+                    with zip_ref.open(file) as zf, open(tmp_target, "wb") as f:
+                        shutil.copyfileobj(zf, f)
+
+
+def unzip_files(path_training, path_test, train_folder, test_folder):
+    if not train_folder.is_dir():
+        log.info("Unzip train...")
+        _unzip_file(path_training)
+    else:
+        log.info("Train dataset is already unzipped")
+    if not test_folder.is_dir():
+        log.info("Unzip test...")
+        _unzip_file(path_test)
+    else:
+        log.info("Test dataset is already unzipped")
+
+
+def move_notebook_files(notebook_folder):
+    if not Path(Path(notebook_folder).parent).joinpath("model.py").is_file():
+        file_names = os.listdir(notebook_folder)
+        for file_name in file_names:
+            shutil.move(
+                os.path.join(notebook_folder, file_name), Path(notebook_folder).parent
+            )
+
+        shutil.rmtree(notebook_folder)
+    else:
+        log.info("Notebooks are also already moved")
+
+
+def split_dataset(train_dataset, target, seed, ratio):
+    # Call the split_folders module (https://github.com/jfilter/split-folders)
+    if not target.is_dir():
+        log.info("Create train/val split")
+        splitfolders.ratio(train_dataset, target, seed=seed, ratio=ratio)
+    else:
+        log.info("Train/val split already exists")
+
+
+def train(litmodel, trainer):
+    trainer.fit(litmodel)
+    return litmodel
+
+
+def test(litmodel, trainer):
+    log.info("Test Result: " + str(trainer.test(litmodel)))
+
+
+def setup_train_env(hparams, mean, std, train_val_folder, test_folder):
+    checkpoint_callback = ModelCheckpoint(
+        monitor="avg_val_acc", save_last=True, save_top_k=1, mode="max"
+    )
+    early_stop_callback = EarlyStopping(
+        monitor="avg_val_acc", min_delta=0.00, patience=5, verbose=False, mode="max"
+    )
+    litmodel = LitModel(hparams, mean, std, train_val_folder, test_folder)
+    trainer = pl.Trainer(
+        gpus=1,
+        fast_dev_run=False,
+        max_epochs=1,
+        progress_bar_refresh_rate=200,
+        default_root_dir="/content/drive/My Drive/Master Thesis/checkpoints",
+        profiler=False,
+        callbacks=[checkpoint_callback, early_stop_callback],
+    )
+
+    return litmodel, trainer
+
+
+if __name__ == "__main__":
+    os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+
+    base_folder = "/content"
+    path_train = "/content/drive/My Drive/Master Thesis/rework/original_training.zip"
+    path_test = "/content/drive/My Drive/Master Thesis/rework/original_test.zip"
+
+    train_folder = Path(base_folder).joinpath(Path(path_train).stem)
+    test_folder = Path(base_folder).joinpath(Path(path_test).stem)
+    train_val_folder = train_folder.joinpath("train_val")
+
+    # Setup files
+    unzip_files(path_train, path_test, train_folder, test_folder)
+    split_dataset(train_folder, train_val_folder, seed=2020, ratio=(0.7, 0.3))
+
+    # Calculate mean
+    mean, std = cal_dir_stat(str(Path(train_val_folder).joinpath("train")), "ppm")
+
+    # Setup model/trainer
+    litmodel, trainer = setup_train_env(
+        hparams, mean, std, train_val_folder, test_folder
+    )
+
+    # Train/test
+    litmodel = train(litmodel, trainer)
+    test(litmodel, trainer)
---
+++ code/model.py
@@ -0,0 +1,250 @@
+from functools import partial
+from pathlib import Path
+
+import pytorch_lightning as pl
+import torch
+import torchvision
+from sklearn.metrics import accuracy_score
+from torch import nn as nn
+from torch import optim as optim
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms as transforms
+
+from deconvolution.models.deconv import FastDeconv
+
+
+class Stn(nn.Module):
+    def __init__(self, setup_dict):
+        super(Stn, self).__init__()
+        # Spatial transformer localization-network
+        self.loc_net = nn.Sequential(
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(
+                setup_dict["conv1_in"],
+                setup_dict["conv1_out"],
+                kernel_size=5,
+                padding=2,
+            ),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(
+                setup_dict["conv2_in"],
+                setup_dict["conv2_out"],
+                kernel_size=5,
+                padding=2,
+            ),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            nn.Flatten(),
+            nn.Linear(setup_dict["lin1_in"], setup_dict["lin1_out"]),
+            nn.ReLU(),
+            nn.Linear(setup_dict["lin2_in"], setup_dict["lin2_out"]),
+        )
+
+    def forward(self, x):
+        xs = self.loc_net(x)
+        theta = xs.view(-1, 2, 3)
+        grid = F.affine_grid(theta, x.size())
+        x = F.grid_sample(x, grid)
+        return x
+
+
+class LitModel(pl.LightningModule):
+    def __init__(self, hparams, mean, std, train_dataset, test_dataset):
+        super(LitModel, self).__init__()
+        self.hparams = hparams
+        self.mean = mean
+        self.std = std
+        self.train_dataset = train_dataset
+        self.test_dataset = test_dataset
+
+        self.activation_function_features = (
+            nn.ELU()
+            if self.hparams.activation_function_features == "ELU"
+            else nn.ReLU()
+        )
+        self.activation_function_features = (
+            nn.ELU()
+            if self.hparams.activation_function_classifier == "ELU"
+            else nn.ReLU()
+        )
+
+        deconv = partial(FastDeconv)
+        self.features = nn.Sequential(
+            # Hidden 1
+            deconv(3, 3, kernel_size=3, stride=1, padding=1),
+            Stn(self.hparams.stn_parameter[0]),
+            nn.Conv2d(3, 200, (7, 7), stride=1, padding=2),
+            self.activation_function_features,
+            nn.MaxPool2d(2, 2),
+            nn.Dropout2d(self.hparams.dropout_rate),
+            nn.BatchNorm2d(200),
+            # Hidden 2
+            Stn(self.hparams.stn_parameter[1]),
+            nn.Conv2d(200, 250, (4, 4), stride=1, padding=2),
+            self.activation_function_features,
+            nn.MaxPool2d(2, 2),
+            nn.Dropout2d(self.hparams.dropout_rate),
+            nn.BatchNorm2d(250),
+            # Hidden 3
+            Stn(self.hparams.stn_parameter[2]),
+            nn.Conv2d(250, 350, (4, 4,), stride=1, padding=2),
+            self.activation_function_features,
+            nn.MaxPool2d(2, 2),
+            nn.Dropout2d(self.hparams.dropout_rate),
+            nn.BatchNorm2d(350),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(12600, 400),
+            self.activation_function_features,
+            nn.Linear(400, 43),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+
+        return x
+
+    def configure_optimizers(self):
+        if self.hparams.optimizer == "SGD":
+            optimizer = optim.SGD(
+                self.parameters(),
+                lr=self.hparams.learning_rate,
+                momentum=self.hparams.momentum,
+            )
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        tensorboard_logs = {"train_loss": loss}
+        return {"loss": loss, "log": tensorboard_logs}
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        a, y_hat = torch.max(y_hat, dim=1)
+        val_acc = accuracy_score(y_hat.cpu(), y.cpu())
+        val_acc = torch.tensor(val_acc)
+
+        return {"val_loss": loss, "val_acc": val_acc}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        avg_val_acc = torch.stack([x["val_acc"] for x in outputs]).mean()
+
+        tensorboard_logs = {"val_loss": avg_loss, "avg_val_acc": avg_val_acc}
+        return {"val_loss": avg_loss, "progress_bar": tensorboard_logs}
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        a, y_hat = torch.max(y_hat, dim=1)
+        test_acc = accuracy_score(y_hat.cpu(), y.cpu())
+
+        return {"test_acc": torch.tensor(test_acc)}
+
+    def test_epoch_end(self, outputs):
+        avg_test_acc = torch.stack([x["test_acc"] for x in outputs]).mean()
+
+        tensorboard_logs = {"avg_test_acc": avg_test_acc}
+        return {
+            "avg_test_acc": avg_test_acc,
+            "log": tensorboard_logs,
+            "progress_bar": tensorboard_logs,
+        }
+
+    def train_dataloader(self):
+        train_set_normal = torchvision.datasets.ImageFolder(
+            root=str(Path(self.train_dataset).joinpath("train")),
+            transform=transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize(self.mean, self.std),]
+            ),
+        )
+
+        loader = DataLoader(
+            train_set_normal, batch_size=50, num_workers=8, shuffle=True
+        )
+
+        return loader
+
+    def val_dataloader(self):
+        val_set_normal = torchvision.datasets.ImageFolder(
+            root=str(Path(self.train_dataset).joinpath("val")),
+            transform=transforms.Compose(
+                [transforms.ToTensor(), transforms.Normalize(self.mean, self.std),]
+            ),
+        )
+
+        val_loader = DataLoader(val_set_normal, batch_size=50, num_workers=8)
+
+        return val_loader
+
+    def test_dataloader(self):
+        test_set_normal = torchvision.datasets.ImageFolder(
+            root=str(self.test_dataset),
+            transform=transforms.Compose(
+                [
+                    # transforms.Grayscale(),
+                    transforms.Resize((48, 48)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(self.mean, self.std),
+                ]
+            ),
+        )
+
+        test_loader = DataLoader(test_set_normal, batch_size=50, num_workers=8)
+
+        return test_loader
+
+
+stn1_params = {
+    "conv1_in": 3,
+    "conv1_out": 250,
+    "conv2_in": 250,
+    "conv2_out": 250,
+    "lin1_in": 250 * 6 * 6,
+    "lin1_out": 250,
+    "lin2_in": 250,
+    "lin2_out": 6,
+}
+
+stn2_params = {
+    "conv1_in": 200,
+    "conv1_out": 150,
+    "conv2_in": 150,
+    "conv2_out": 200,
+    "lin1_in": 200 * 2 * 2,
+    "lin1_out": 300,
+    "lin2_in": 300,
+    "lin2_out": 6,
+}
+
+stn3_params = {
+    "conv1_in": 250,
+    "conv1_out": 150,
+    "conv2_in": 150,
+    "conv2_out": 200,
+    "lin1_in": 200 * 1 * 1,
+    "lin1_out": 300,
+    "lin2_in": 300,
+    "lin2_out": 6,
+}
+
+stn_params = [stn1_params, stn2_params, stn3_params]
+
+hparams = {
+    "dropout_rate": 0.45,
+    "learning_rate": 0.001,
+    "momentum": 0,
+    "optimizer": "SGD",
+    "activation_function_features": "RELU",
+    "activation_function_classifier": "RELU",
+    "stn_parameter": stn_params,
+}
